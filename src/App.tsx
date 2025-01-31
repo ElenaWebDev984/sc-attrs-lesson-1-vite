59c745d9460e93ece7b5f4e1d6e2875f0edef027
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-01 00:46:36 +0100

    add props

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ function App() {
             <Title>Styled-components <span>.attrs</span> method</Title>
 
             <Form>
-                <Field type="password"/>
-                <Field type="radio"/>
-                <Field type="checkbox"/>
+                <Field placeholder={"Hello!!!"}/>
+                <Field title={"Type hello here"}/>
+                <Field />
+                <Checkbox />
             </Form>
         </div>
     );
@@ -18,21 +19,32 @@ function App() {
 
 export default App;
 
+const Checkbox = styled.input.attrs(()=>({
+    type: 'checkbox',
+}))`
+// styles here...
+`;
+
 const Title = styled.h1`
     font-size: 1.5em;
     text-align: center;
     margin-bottom: 32px;
+
     span {
         color: #e91e63;
     }
 `;
 
 const Form = styled.form`
-  width: 100%;
-  max-width: 500px;
+    width: 100%;
+    max-width: 500px;
 `;
 
-const Field = styled.input`
+const Field = styled.input.attrs(({placeholder, title})=>({
+    type: "password",
+    placeholder: placeholder || "Type smth...",
+    title: title || "Fill this field, please..."
+}))`
   padding: 5px 15px;
   margin: 10px 0;
   width: 100%;
